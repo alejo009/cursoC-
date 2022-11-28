f318c5646bdeacce995999a46b48f30bc222ad8d
alejo_jimenez <alejandro.jimenez@tekus.co>
2022-11-27 22:03:49 -0500

    Metodos void

---
+++ MetodoVoid/MetodoVoid/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoVoid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            EscribeAlgo();
+            EscribeAlgoEspecifico("Soy un argumento y me llamaron y me llamaron desde el metodo Main");
+        }
+
+
+        //nivel de acceso - Static - tipoRetorno - Nombre del metodo - parametros
+        public static void EscribeAlgo()
+        {
+            Console.WriteLine("Hello world");
+        }
+
+        public static void EscribeAlgoEspecifico(string miTexto)
+        {
+            Console.WriteLine(miTexto);
+        }
+    }
+}
